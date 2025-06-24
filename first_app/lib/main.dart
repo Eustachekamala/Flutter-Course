e57c50dc1c0beed18a4c6446ed86aa9a44163cde
eustachekamala <eustachejared25@gmail.com>
2025-06-24 14:13:50 +0300

    work with the style

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,39 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp( const AppBarApp());
-}
-
-class AppBarApp extends StatelessWidget {
-  const AppBarApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: AppBarExample(),);
-  }
-
-}
-
-class AppBarExample extends StatelessWidget{
-  const AppBarExample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
       appBar: AppBar(
-        title: const Text('FluxUI'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.add_alert),
-            tooltip: 'Show Snackbar',
-            onPressed: () {
-              ScaffoldMessenger.of(
-                context,
-              ).showSnackBar(const SnackBar(content: Text('This is a snackbar')));
-            },
-          )
-        ],
+        title: Center(
+          child: Text(
+              'FlowUX',
+            style: TextStyle(
+              color: Colors.white
+            ),
+          ),
+        ),
+        backgroundColor: Colors.black,
       ),
-    );
-  }
+      body: Center(
+        child: Text(
+          'Welcome Home',
+          style: TextStyle(
+            color: Colors.white
+          ),
+        ),
+      ),
+      backgroundColor: Colors.black,
+    ),
+  ));
 }
-
