021d7cb1b26f8c53cf8936197ef6d60706e2dabc
eustachekamala <eustachejared25@gmail.com>
2025-06-26 15:02:32 +0300

    used input widget to the the message

---
+++ chat_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:chat_app/pages/todo_page.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ToDoPage(),
+    );
+  }
+}
